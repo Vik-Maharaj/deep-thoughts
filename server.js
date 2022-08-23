38d3f8f91ba51f7589d30cbf25581c80e0a9fbb5
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-08-23 12:15:33 -0400

    Add middleware and server init function

--- server.js
+++ server.js
@@ -10,8 +10,22 @@ const server = new ApolloServer({
   resolvers
 });
 
-
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+ 
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
 
 startApolloServer(typeDefs, resolvers);
