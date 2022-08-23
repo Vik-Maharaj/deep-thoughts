807b3432a5ca382118c7578deb81cbebfda9d385
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-08-23 12:10:14 -0400

    Modify server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
 
+const app = express();
 
 
 startApolloServer(typeDefs, resolvers);
