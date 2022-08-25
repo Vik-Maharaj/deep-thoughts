97fcda1140741aca2027bc89ec409a681592d853
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-08-25 19:53:09 -0400

    Add thought model

---
+++ models/Thought.js
@@ -0,0 +1,8 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dateFormat = require('../utils/dateFormat');
+
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
