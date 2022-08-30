469d171737194e6dd3199b113dd7f402b75dc0a4
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-08-30 13:18:50 -0400

    Add server.js middleware and require auth

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -14,8 +16,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
- 
-// Create a new instance of an Apollo server with the GraphQL schema
+
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
@@ -27,5 +28,5 @@ const startApolloServer = async (typeDefs, resolvers) => {
     })
   })
   };
-
-startApolloServer(typeDefs, resolvers);
+  
+  startApolloServer(typeDefs, resolvers);
