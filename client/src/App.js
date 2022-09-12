8290d6d13e0ed957787c989e3cbb38288c033b90
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-09-12 14:16:03 -0400

    Update app.js to add correct apollo routes

--- client/src/App.js
+++ client/src/App.js
@@ -34,9 +34,29 @@ function App() {
           <Header />
           <div className="container">
             <Routes>
-
-
-
+              <Route 
+                path="/" 
+                element={<Home />} 
+              />
+              <Route 
+                path="/login" 
+                element={<Login />} 
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />} 
+              />
+              <Route 
+                path="/profile" 
+                element={<Profile />} 
+              />
+              <Route 
+                path="/thought/:id" 
+                element={<SingleThought />} 
+              />
+              <Route 
+                path="*" 
+                element={<NoMatch />} 
               />
             </Routes>
           </div>
