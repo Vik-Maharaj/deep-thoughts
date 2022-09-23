b346e7a82842090096d0031d63ae1f8e718290ba
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-09-23 13:15:10 -0400

    Add thoughtform component

---
+++ client/src/components/ThoughtForm/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+import { useMutation } from '@apollo/client';
+import { ADD_THOUGHT } from '../../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
+
+const ThoughtForm = () => {
+
+  
+};
+
+export default ThoughtForm;
